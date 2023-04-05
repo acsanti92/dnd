500119484e3dcf90e8daeb28e93663610775e09c
acsanti92 <armando.santiago@pluto.tv>
2023-04-05 15:22:02 -0700

    Created Character Class and Ability

--- dungeons-dragons/Assets/Scripts/Character.cs
+++ dungeons-dragons/Assets/Scripts/Character.cs
@@ -1,18 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour
+// These are the six ability scores that are used to determine a character's attributes.
+public enum CharacterAbility
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Strength,
+    Dexterity,
+    Constitution,
+    Intelligence,
+    Wisdom,
+    Charisma
+}
+[Serializable]
 
-    // Update is called once per frame
-    void Update()
+// This is a class that holds the ability score and the score itself.
+public class Ability
+{
+    public int abilityScore;
+    public CharacterAbility ability;
+
+    public Ability(CharacterAbility ability, int abilityScore)
     {
-        
+        this.ability = ability;
+        this.abilityScore = abilityScore;
+    }
+}
+
+[CreateAssetMenu]
+
+public class Character : ScriptableObject
+{
+    // List of abilities that the character has.
+    public List<Ability> abilities;
+
+    // This is called when the script is loaded or a value is changed in the inspector.
+    void Reset()
+    {
+        abilities = new List<Ability>();
+        abilities.Add(new Ability(CharacterAbility.Strength, 10));
+        abilities.Add(new Ability(CharacterAbility.Dexterity, 10));
+        abilities.Add(new Ability(CharacterAbility.Constitution, 10));
+        abilities.Add(new Ability(CharacterAbility.Intelligence, 10));
+        abilities.Add(new Ability(CharacterAbility.Wisdom, 10));
+        abilities.Add(new Ability(CharacterAbility.Charisma, 10));
     }
 }
